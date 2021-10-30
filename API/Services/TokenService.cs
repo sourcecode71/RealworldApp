ee205cea9cbf45564fe0f3515a0892af273c156f
Vojislav Dimitrijevic <vojislav.dimitrijevic@elfak.rs>
2021-10-30 17:22:47 +0200

    Authorization policy

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -6,12 +6,19 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService
     {
+        private readonly IConfiguration _config;
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public string CreateToken(Employee employee)
         {
             var claims = new List<Claim>
@@ -19,7 +26,7 @@ namespace API.Services
                 new Claim(ClaimTypes.Email, employee.Email),
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
