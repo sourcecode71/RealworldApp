7cb878120a87f227b07d3e83dcd422381a337f73
Stefan Djokic <stefan.djokic1995@gmail.com>
2021-11-21 01:59:23 +0100

    Separate pages for Add Employee and Add Project

--- Domain/Enums/ProjectStatus.cs
+++ Domain/Enums/ProjectStatus.cs
@@ -2,10 +2,11 @@
 {
     public enum ProjectStatus
     {
-        OnTime = 1,
+        OnTime = 1, //InProgress
         Delayed = 2,
         Modified =3, 
         Archived = 4,
-        Completed = 5
+        Completed = 5,
+        Invoiced = 6
     }
 }
