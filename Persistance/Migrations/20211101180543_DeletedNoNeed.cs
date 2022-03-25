bc6b68924d351d17f43407a6b2d6b1f309c75005
sourcecode71 <mostafiz571@gmail.com>
2022-03-25 19:59:30 +0600

    update plexo live db

--- Persistance/Migrations/20211101180543_DeletedNoNeed.cs
+++ Persistance/Migrations/20211101180543_DeletedNoNeed.cs
@@ -6,40 +6,40 @@ namespace Persistance.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "EmployeeProject");
+            ////migrationBuilder.DropTable(
+            ////    name: "EmployeeProject");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "EmployeeProject",
-                columns: table => new
-                {
-                    EmployeesId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    ProjectsId = table.Column<string>(type: "nvarchar(450)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_EmployeeProject", x => new { x.EmployeesId, x.ProjectsId });
-                    table.ForeignKey(
-                        name: "FK_EmployeeProject_AspNetUsers_EmployeesId",
-                        column: x => x.EmployeesId,
-                        principalTable: "AspNetUsers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_EmployeeProject_Projects_ProjectsId",
-                        column: x => x.ProjectsId,
-                        principalTable: "Projects",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "EmployeeProject",
+            //    columns: table => new
+            //    {
+            //        EmployeesId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+            //        ProjectsId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_EmployeeProject", x => new { x.EmployeesId, x.ProjectsId });
+            //        table.ForeignKey(
+            //            name: "FK_EmployeeProject_AspNetUsers_EmployeesId",
+            //            column: x => x.EmployeesId,
+            //            principalTable: "AspNetUsers",
+            //            principalColumn: "Id",
+            //            onDelete: ReferentialAction.Cascade);
+            //        table.ForeignKey(
+            //            name: "FK_EmployeeProject_Projects_ProjectsId",
+            //            column: x => x.ProjectsId,
+            //            principalTable: "Projects",
+            //            principalColumn: "Id",
+            //            onDelete: ReferentialAction.Cascade);
+            //    });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_EmployeeProject_ProjectsId",
-                table: "EmployeeProject",
-                column: "ProjectsId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_EmployeeProject_ProjectsId",
+            //    table: "EmployeeProject",
+            //    column: "ProjectsId");
         }
     }
 }
---
+++ Web/wwwroot/views/AdminPanelEmp.js
@@ -0,0 +1,210 @@
+﻿const app = new Vue({
+    el: '#archiveDiv',
+    beforeMount() {
+        this.loadAllWorkOrder();
+    },
+    data: {
+        errors: [],
+        workOrders: [],
+        wrkS: '0',
+        allInv: false,
+        allHrs: false,
+        isHrsDetails: false,
+        projectNo: '',
+        consProject: '',
+        projectYear: '',
+        wrkNo: '',
+        wrkConsWork: '',
+        wrkBudgetNo: '',
+        wrkStartDate: '',
+        wrkEndDate: '',
+        sbudget: '',
+        abudget: '',
+        ahours: '',
+        companyName: '',
+        description: '',
+        bhours: '',
+        ahours: '',
+        sbudget: '',
+        abudget: '',
+        paidAmt: 0,
+        clientName: '',
+        wrkStatusStr: '',
+        assignEmp: [],
+        empHrs:[]
+    },
+    methods: {
+
+       
+        loadAllWorkOrder: function () {
+
+            const config = { headers: { "Content-Type": "application/json" } };
+            var base_url = window.location.origin;
+            const clientURL = base_url + "/api/WorkOrder/load-work-orders/active";
+
+            axios.get(clientURL, config).then(
+                (result) => {
+
+                    $("#wrkAdminPanel").dataTable().fnDestroy();
+
+                    this.workOrders = result.data;
+
+                    setTimeout(() => {
+                        $('#wrkAdminPanel').DataTable({
+                            "scrollY": "500px",
+                            "scrollCollapse": true,
+                            "paging": false,
+                            "columns": [
+                                { "width": "2%" },
+                                { "width": "5%" },
+                                { "width": "9%" },
+                                { "width": "11%" },
+                                { "width": "9%" },
+                                { "width": "20%" },
+                                { "width": "10%" },
+                                { "width": "10%" }
+                            ]
+                        });
+                    }, 100);
+
+                    console.log(" datata ", this.workOrders);
+
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+
+        },
+
+        AssignInfo: function (wrk) {
+            this.wrk = wrk;
+            this.projectYear = wrk.projectYear;
+            this.projectNo = wrk.projectNo;
+            this.consProject = wrk.projectName;
+
+            this.wrkNo = wrk.workOrderNo;
+            this.wrkStartDate = wrk.startDateStr;
+            this.wrkEndDate = wrk.endDateStr;
+            this.wrkBudgetNo = wrk.wrkBudgetNo;
+            this.wrkConsWork = wrk.consecutiveWork;
+            this.bhours = wrk.budgetHour;
+            this.ahours = wrk.spentHour;
+            this.sbudget = wrk.originalBudget;
+            this.abudget = wrk.approvedBudget;
+            this.clientName = wrk.clientName;
+            this.companyName = wrk.companyName;
+            this.description = wrk.otDescription;
+            this.paidAmt = wrk.approvedBudget - wrk.balance;
+            this.wrkStatusStr = wrk.wrkStatusStr;
+        },
+
+        ShowAllInvoice: function (wrk) {
+            this.AssignInfo(wrk);
+            this.allInv = true;
+            this.allHrs = false;
+
+        },
+
+        ShowAllHRS: function (wrk) {
+            this.AssignInfo(wrk);
+            this.allInv = false;
+            this.allHrs = true;
+            this.LoadHoursLogSummery(wrk.id);
+
+        },
+
+        LoadHoursLogSummery: function (wrkId) {
+            const config = { headers: { "Content-Type": "application/json" } };
+            var base_url = window.location.origin;
+            const wrkURL = base_url + "/api/Employee/workorder/emp-hour-summery?wrkId=" + wrkId;
+
+            axios.get(wrkURL, config).then(
+                (result) => {
+                    this.assignEmp = result.data;
+
+                    console.log("  this.assignEmp ", this.assignEmp);
+                },
+                (error) => {
+                    console.error(error);
+                });
+
+        },
+
+        LoadHoursLogDetails: function (wrkId) {
+            const config = { headers: { "Content-Type": "application/json" } };
+            var base_url = window.location.origin;
+            const wrkURL = base_url + "/api/Employee/workorder/emp-hour-details?wrkId=" + wrkId;
+
+            axios.get(wrkURL, config).then(
+                (result) => {
+
+                    $("#empHrsDetails").dataTable().fnDestroy();
+
+                    setTimeout(() => {
+                        this.empHrsDetails = result.data;
+                    }, 100);
+
+                    setTimeout(() => {
+                        $("#empHrsDetails").DataTable({
+                            scrollY: "500px",
+                            scrollCollapse: true,
+                            paging: false,
+                        });
+                    }, 500);
+
+
+
+                    //empHrsDetails
+                },
+                (error) => {
+                    console.error(error);
+                });
+
+        },
+        EmpHoursLogDetails: function (empid) {
+
+            this.isHrsDetails = true;
+
+            const config = { headers: { "Content-Type": "application/json" } };
+            var base_url = window.location.origin;
+            const wrkURL = base_url + "/api/Employee/emp-wise-hour?empId=" + empid;
+
+            axios.get(wrkURL, config).then(
+                (result) => {
+
+                    console.log(" emp hrs ", this.empHrs);
+
+                    this.empHrs = result.data;
+                },
+                (error) => {
+                    console.error(error);
+                });
+
+        },
+
+        empTotalLog: function (hrs) {
+            var total_amount = 0;
+            $.each(hrs, function (i, v) { total_amount += v.lhour; });
+            return total_amount;
+        },
+
+        showInvoiceDetails: function (wrk) {
+            var base_url = window.location.origin;
+            const wrkURL = base_url + "/Dashboard/InvoiceDetails?wrkId=" + wrk.id;
+            window.location = wrkURL;
+        },
+
+        formatCurrency: function (Crn) {
+            let dollarUS = Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+            });
+
+            return  dollarUS.format(Crn);
+            
+        }
+  }
+
+
+});
