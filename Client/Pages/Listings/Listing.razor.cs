2fc21c858113235d2d01a53ab21c45785a55f906
Mostafiz <mostafiz57@gmail.com>
2022-04-17 08:06:57 +0600

    add partial class

---
+++ Client/Pages/Listings/Listing.razor.cs
@@ -0,0 +1,41 @@
+﻿using RealworldApp.Api.Model.Listings;
+using RealworldApp.Client.Components.Listings;
+using System.Text;
+using System.Text.Json;
+
+namespace RealworldApp.Client.Pages.Listings
+{
+    public partial class Listing
+    {
+        ListingModel lgVM = new ListingModel();
+        BrandingModel bndVM = new BrandingModel();
+        PackageModel pkgVM = new PackageModel();
+        PackagingComponent pkgComponent;
+        BrandingComponent bndComponent;
+
+
+        protected async Task InvalidFormSubmitted()
+        {
+            if (bndComponent.CanSubmitBranding() & pkgComponent.CanSubmitPackage())
+            {
+
+            }
+        }
+
+        protected async Task ValidFormSubmitted()
+        {
+            var content = JsonSerializer.Serialize(lgVM);
+
+            //var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            //var postResult = await _client.PostAsync("products", bodyContent);
+            //var postContent = await postResult.Content.ReadAsStringAsync();
+            //if (!postResult.IsSuccessStatusCode)
+            //{
+            //    throw new ApplicationException(postContent);
+            //}
+
+
+        }
+
+    }
+}
